257c5bbf9fe7e19529be8a1c78569f41cc5d6de2
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-04-03 23:54:39 +0530

    Feature: added menu API

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ import googleAuthConfig from "./config/google.config";
 import Auth from "./API/Auth";
 import Restaurant from "./API/Restaurant";
 import Food from "./API/Food";
+import Menu from "./API/Menu";
 
 // Database connection
 import ConnectDB from "./database/connection";
@@ -46,6 +47,7 @@ googleAuthConfig(passport);
 zomato.use("/auth", Auth);
 zomato.use("/restaurant", Restaurant);
 zomato.use("/food", Food);
+zomato.use("/menu", Menu);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
