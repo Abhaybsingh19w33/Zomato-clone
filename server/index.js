d34bba939bb2c13bc8bb8602309e1c51b74c32d4
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-04-04 10:25:34 +0530

    Feature: Added Order API

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ import Restaurant from "./API/Restaurant";
 import Food from "./API/Food";
 import Menu from "./API/Menu";
 import Image from "./API/Image";
+import Order from "./API/orders";
 
 // Database connection
 import ConnectDB from "./database/connection";
@@ -50,6 +51,7 @@ zomato.use("/restaurant", Restaurant);
 zomato.use("/food", Food);
 zomato.use("/menu", Menu);
 zomato.use("/image", Image);
+zomato.use("/order", Order);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
