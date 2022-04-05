fbbc8d4e855fd3545e10cd31547b3d50f2d7b8ac
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-04-06 00:29:51 +0530

    Feature: Added deliveryCarousel using delievery category to create food template

--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -2,11 +2,20 @@ import React, { Fragment } from "react";
 // useParams - it can access url parameters
 import { useParams } from "react-router-dom";
 
+// components
+import Delivery from "./Delivery";
+
 const Master = () => {
     // extracting type parameter from url
     const { type } = useParams();
 
-    return <Fragment>{type}</Fragment>;
+    return (
+        <Fragment>
+            <div className="my-5">
+                {type === "delivery" && <Delivery />}
+            </div>
+        </Fragment>
+    );
 };
 
 export default Master;
