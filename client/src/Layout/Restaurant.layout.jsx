2f7519b47387e11f2dea129d43faafba1f5bdd80
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-04-08 16:23:48 +0530

    Feature: Add menu images

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,4 +1,6 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { TiStarOutline } from "react-icons/ti";
 import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
 import { BiBookmarkPlus } from "react-icons/bi";
@@ -11,13 +13,41 @@ import RestaurantInfo from "../Components/restaurant/RestaurantInfo";
 import TabContainer from "../Components/restaurant/Tabs";
 import CartContainer from "../Components/Cart/CartContainer";
 
+// Redux actions
+import { getSpecificRestaurant } from "../Redux/Reducer/restaurant/restaurant.action";
+import { getImage } from "../Redux/Reducer/Image/Image.action";
+
 const RestaurantLayout = (props) => {
+
+    const [restaurant, setRestaurant] = useState({
+        images: [],
+        name: "",
+        cuising: "",
+        address: "",
+    });
+    const { id } = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getSpecificRestaurant(id)).then((data) => {
+            setRestaurant((prev) => ({
+                ...prev,
+                ...data.payload.restaurant,
+            }));
+
+            dispatch(getImage(data.payload.restaurant.photos)).then((data) =>
+                setRestaurant((prev) => ({ ...prev, ...data.payload.image }))
+            );
+        });
+    }, []);
+
+
     return (
         <Fragment>
             {" "}
             <RestaurantNavbar />
             <div className="container mx-auto px-4 lg:px-20 ">
-                <ImageGrid
+                {/* <ImageGrid
                     images={[
                         "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
                         "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
@@ -25,13 +55,21 @@ const RestaurantLayout = (props) => {
                         "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
                         "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
                     ]}
-                />
-                <RestaurantInfo
+                /> */}
+                <ImageGrid images={restaurant.images} />
+                {/* <RestaurantInfo
                     name="Mumbai Xpress"
                     restaurantRating="3.5"
                     deliveryRating="3.2"
                     cuisine="North Indian, Fast Food, Chinese, Street Food"
                     address="Basavanagudi, Bangalore"
+                /> */}
+                <RestaurantInfo
+                    name={restaurant?.name}
+                    restaurantRating={restaurant?.rating || 0}
+                    deliveryRating={restaurant?.rating || 0}
+                    cuisine={restaurant?.cuising}
+                    address={restaurant?.address}
                 />
                 <div className="my-4 flex flex-wrap gap-3">
                     <InfoButtons isActive>
--- client/src/Redux/Reducer/restaurant/restaurant.type.js
+++ client/src/Redux/Reducer/restaurant/restaurant.type.js
@@ -1,2 +1,4 @@
 // in this file cases are declared
 export const GET_RESTAURANT = "GET_RESTAURANT";
+
+export const GET_SPECIFIC_RESTAURANT = "GET_SPECIFIC_RESTAURANT";
