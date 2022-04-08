6ef9a1d0b2961738a5161c7a76b54344fe5f5831
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-04-08 17:32:32 +0530

    Feature: add review reducers

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -46,7 +46,7 @@ const RestaurantLayout = (props) => {
         <Fragment>
             {" "}
             <RestaurantNavbar />
-            <div className="container mx-auto px-4 lg:px-20 ">
+            <div className="container mx-auto px-4 lg:px-20 pb-10">
                 {/* <ImageGrid
                     images={[
                         "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
